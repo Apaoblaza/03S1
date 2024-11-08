39161e480c464563a8bcc210083e760da0280705
Pablo Apaoblaza <pablo.apaoblaza@gmail.com>
2024-11-08 20:32:57 -0300

    Desafio 03S1

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-precio = 400000
-
+precio = 400000;
+qty = 0;
 precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+precioSpan.innerHTML = precio;
+cantidadSpan = document.querySelector(".cantidad");
+valorTotalSpan = document.querySelector(".valor-total");
+upbutton = document.getElementById("upbutton");
+downbutton = document.getElementById("downbutton");
+
+// function agrega() {
+//   qty++;
+//   valortotal = precio * qty;
+//   cantidadSpan.innerHTML = qty;
+//   valorTotalSpan.innerHTML = valortotal;
+// }
+
+// upbutton.addEventListener("click", agrega);
+
+// function quita() {
+//   qty--;
+//   valortotal = precio * qty;
+//   cantidadSpan.innerHTML = qty;
+//   valorTotalSpan.innerHTML = valortotal;
+//   valortotal = precio;
+// }
+
+// downbutton.addEventListener("click", quita);
